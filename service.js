2594cf53c9b02b2b2fe35eeaffa7b487b4634206
LaddMauigoa <laddmauigoa@gmail.com>
2015-01-24 11:14:23 -0700

    done changed last names too

--- service.js
+++ service.js
@@ -2,11 +2,20 @@ var app = angular.module('userProfiles');
 
 app.service('mainService', function($http, $q) {
 
-
+	var deferred = $q.defer();
   this.getUsers = function() {
-    return $http({
+    $http({
         method: 'GET',
         url: 'http://reqr.es/api/users?page=1'
+    }).then(function(response) {
+    	response = response.data.data
+        console.log(response)
+    	for(i = 0; i < response.length; i++) {
+    		response[i].first_name = 'Ralf'
+            response[i].last_name = 'not ralffff'
+    	}
+    	deferred.resolve(response)
     })
+    return deferred.promise;
   }
 });
